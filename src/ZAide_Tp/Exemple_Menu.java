d6984d95be788cfae950fbf756248db209847b68
RBabari <r.babari@gmail.com>
2025-09-23 14:53:37 -0400

    exemple menu

---
+++ src/ZAide_Tp/Exemple_Menu.java
@@ -0,0 +1,18 @@
+package ZAide_Tp;
+
+public class Exemple_Menu {
+    public static void main(String[] args) {
+        String menu =
+                "******************************************\n" +
+                "*         Bienvenue au complexe Sportif    BDEB           *\n" +
+                "*                Catégorie des abonnements:                     *\n" +
+                "*  ----------------------------                                        *                            \n" +
+                "*    F       Forfait  (Nombre d'activités illimité)          *                            \n" +
+                "*    A      Activités     (Nombre limité)                        *\n" +
+                "******************************************\n" +
+                "Choisissez le type d'abonnement :  \n";
+
+        System.out.println(menu);
+
+    }
+}
---
+++ src/ZAide_Tp/Exemple_print.java
@@ -0,0 +1,10 @@
+package ZAide_Tp;
+
+public class Exemple_print {
+    public static void main(String[] args) {
+        // \n saute une ligne
+        System.out.print("*** \n");
+        System.out.print("\n*** \n ");
+    }
+
+}
